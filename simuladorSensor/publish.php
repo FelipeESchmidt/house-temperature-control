b097d6ce949a3e2d2f427410ffd8f58ff92af87e
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-10-13 22:49:59 -0300

    feat: adjusting simulator code

--- simuladorSensor/publish.php
+++ simuladorSensor/publish.php
@@ -1,40 +1,38 @@
 <?php
 
-    require('phpMQTT-master/phpMQTT.php');
-    date_default_timezone_set('America/Sao_Paulo');
+require('phpMQTT-master/phpMQTT.php');
+date_default_timezone_set('America/Sao_Paulo');
 
-    $qtd = (isset($_GET['qtd'])) ? $_GET['qtd'] : 20;
-    $i = 0;
-    $j = 0;
-    
-    $server = 'test.mosquitto.org';             // change if necessary
-    $port = 1883;                               // change if necessary
-    $username = '';                             // set your username
-    $password = '';                             // set your password
-    $client_id = 'PHPcontainerGURIpublish';     // make sure this is unique for connecting to sever - you could use uniqid()
+$qtd = (isset($_GET['qtd'])) ? $_GET['qtd'] : 20;
+$i = 0;
+$j = 0;
 
-    $mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
+$server = 'test.mosquitto.org';             // change if necessary
+$port = 1883;                               // change if necessary
+$username = '';                             // set your username
+$password = '';                             // set your password
+$client_id = 'PHPcontainerGURIpublish';     // make sure this is unique for connecting to sever - you could use uniqid()
 
-    if ($mqtt->connect(true, NULL, $username, $password)) { 
-        while($i<$qtd){
-            for ($j=1; $j <= 4; $j++) {
-                postMqtt($mqtt, $j);
-            }
-            $i++;
-            echo "***********************************************************************\n\n";
-            sleep(2);
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
+
+if ($mqtt->connect(true, NULL, $username, $password)) {
+    while ($i < $qtd) {
+        for ($j = 1; $j <= 4; $j++) {
+            postMqtt($mqtt, $j);
         }
-        $mqtt->close();
-    } else {
-        echo "Não foi possível conectar o servidor!\n";
+        $i++;
+        echo "***********************************************************************\n\n";
+        sleep(2);
     }
+    $mqtt->close();
+} else {
+    echo "Não foi possível conectar o servidor!\n";
+}
 
-    function postMqtt($mqtt, $sensor_number){
-        $sensor = "Sensor_$sensor_number";
-        $temperatura = random_int(28, 32);
-        $data = date("d/m/Y H:i:s");
-        $mqtt->publish('SensoresGURIsd', "{ \"sensorNumber\": $sensor_number, \"temperatura\": $temperatura, \"data\": \"$data\" }", 0, false);
-        echo "sending: { \"sensorNumber\": $sensor_number, \"temperatura\": $temperatura, \"data\": $data }\n\n";
-    }
-
-?>
+function postMqtt($mqtt, $number)
+{
+    $temperature = random_int(28, 32);
+    $date = date("d/m/Y H:i:s");
+    $mqtt->publish('SensoresGURImc', "{ \"\": $number, \"temperature\": $temperature, \"date\": \"$date\" }", 0, false);
+    echo "sending: { \"\": $number, \"temperature\": $temperature, \"date\": $date }\n\n";
+}
