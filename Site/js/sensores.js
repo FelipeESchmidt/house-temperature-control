518a4d3036a99b00792b165ce9a3d296ef042d5d
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-10-13 23:00:53 -0300

    feat: adjusting html app to work correctly

--- Site/js/sensores.js
+++ Site/js/sensores.js
@@ -18,13 +18,13 @@ $(function () {
 
   function changeTemps(sensores) {
     var sens = Object.values(sensores.Sensor_1);
-    changeCSS("sensor_1", sens[sens.length - 1].temperatura);
+    changeCSS("sensor_1", sens[sens.length - 1].temperature);
     var sens = Object.values(sensores.Sensor_2);
-    changeCSS("sensor_2", sens[sens.length - 1].temperatura);
+    changeCSS("sensor_2", sens[sens.length - 1].temperature);
     var sens = Object.values(sensores.Sensor_3);
-    changeCSS("sensor_3", sens[sens.length - 1].temperatura);
+    changeCSS("sensor_3", sens[sens.length - 1].temperature);
     var sens = Object.values(sensores.Sensor_4);
-    changeCSS("sensor_4", sens[sens.length - 1].temperatura);
+    changeCSS("sensor_4", sens[sens.length - 1].temperature);
   }
 
   function changeCSS(sensor, temp) {
--- Site/pages/js/sensor.js
+++ Site/pages/js/sensor.js
@@ -1,49 +1,58 @@
-$(function(){
+$(function () {
+  var sensores;
+  var sensor = $("sensor").attr("name");
 
-    var sensores;
-    var sensor = $('sensor').attr('name');
-
+  repit();
+  setInterval(function () {
     repit();
-    setInterval(function(){
-        repit();
-    },1000);
+  }, 1000);
 
-    function repit(){
+  function repit() {
+    console.log(sensor);
+    $.getJSON(`${getDatabaseURL()}${sensor}.json`, function (data) {
+      sensores = data;
+    });
 
-        $.getJSON('LINK_DO_SEU_BANCO_FIREBASE'+sensor+'.json', function(data) {
-            sensores = data;
-        });
-
-        if (sensores != null) {   
-            changeTemps(sensores);
-            putTemperatures(sensores);
-            
-        }
-
+    if (sensores != null) {
+      changeTemps(sensores);
+      putTemperatures(sensores);
     }
+  }
 
-    function changeTemps(sensores){
-        var sens = Object.values(sensores);
-        changeCSS("sensor_1", sens[sens.length-1].temperatura);
-        var sens = Object.values(sensores);
-        changeCSS("sensor_2", sens[sens.length-1].temperatura);
-        var sens = Object.values(sensores);
-        changeCSS("sensor_3", sens[sens.length-1].temperatura);
-        var sens = Object.values(sensores);
-        changeCSS("sensor_4", sens[sens.length-1].temperatura);
-    }
+  function changeTemps(sensores) {
+    var sens = Object.values(sensores);
+    changeCSS("sensor_1", sens[sens.length - 1].temperature);
+    var sens = Object.values(sensores);
+    changeCSS("sensor_2", sens[sens.length - 1].temperature);
+    var sens = Object.values(sensores);
+    changeCSS("sensor_3", sens[sens.length - 1].temperature);
+    var sens = Object.values(sensores);
+    changeCSS("sensor_4", sens[sens.length - 1].temperature);
+  }
 
-    function changeCSS(sensor, temp){
-        $('#'+sensor).find('div').attr('temp','temp_'+temp);
-        $('#'+sensor).find('div').find('span').html(temp+'º');
-    }
+  function changeCSS(sensor, temp) {
+    $("#" + sensor)
+      .find("div")
+      .attr("temp", "temp_" + temp);
+    $("#" + sensor)
+      .find("div")
+      .find("span")
+      .html(temp + "º");
+  }
 
-    function putTemperatures(sensores){
-        all = Object.values(sensores);
-        $('tbody').html('');
-        all.forEach(medicao => {
-            $('tbody').prepend('<tr class="temp_color" temp="temp_'+medicao.temperatura+'"><td>'+medicao.temperatura+'º</td><td>'+medicao.data+'</td></tr>');
-        });
-    }
-	
+  function putTemperatures(sensores) {
+    all = Object.values(sensores);
+    $("tbody").html("");
+    all.forEach((data) => {
+      $("tbody").prepend(
+        '<tr class="temp_color" temp="temp_' +
+          data.temperature +
+          '"><td>' +
+          data.temperature +
+          "º</td><td>" +
+          data.date +
+          "</td></tr>"
+      );
+    });
+  }
 });
