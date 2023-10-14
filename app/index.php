161a363c29133e11bc2c64f0e7bc5e5c52fef565
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-10-13 22:42:33 -0300

    feat: fixing subscribe code and creating a hidden file with database url

--- app/index.php
+++ app/index.php
@@ -1,3 +1,2 @@
 <?php
-    require('subscribe.php');
-?>
+require('subscribe.php');
--- app/subscribe.php
+++ app/subscribe.php
@@ -1,60 +1,66 @@
 <?php
 
-    require('phpMQTT-master/phpMQTT.php');
+require('./phpMQTT-master/phpMQTT.php');
+require('./database.php');
 
-    $server = 'test.mosquitto.org';             // change if necessary
-    $port = 1883;                               // change if necessary
-    $username = '';                             // set your username
-    $password = '';                             // set your password
-    $client_id = 'PHPcontainerGURIsubscribe';   // make sure this is unique for connecting to sever - you could use uniqid()
+$server = 'test.mosquitto.org';             // change if necessary
+$port = 1883;                               // change if necessary
+$username = '';                             // set your username
+$password = '';                             // set your password
+$client_id = 'PHPcontainerGURIsubscribe';   // make sure this is unique for connecting to sever - you could use uniqid()
 
-    $i = 0;
+$i = 0;
 
-    $mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
-    if(!$mqtt->connect(true, NULL, $username, $password)) {
-        exit(1);
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
+if (!$mqtt->connect(true, NULL, $username, $password)) {
+    exit(1);
+}
+
+$mqtt->debug = true;
+
+$topics['SensoresGURImc'] = array('qos' => 0, 'function' => 'postFirebase');
+$mqtt->subscribe($topics, 0);
+
+while ($mqtt->proc() && $i < 400) {
+    $i++;
+}
+
+$mqtt->close();
+
+function postFirebase($topic, $msg)
+{
+    $sensorMessage = json_decode($msg);
+    $isValid = validateObject($sensorMessage);
+    echo $isValid;
+    if ($isValid) {
+        $sensor = "Sensor_$sensorMessage->number";
+        $temperature = $sensorMessage->temperature;
+        $date = $sensorMessage->date;
+        sendCurl($sensor, $temperature, $date);
+        echo "Mensagem recebida com sucesso, e repassada para Firebase\n";
+    } else {
+        echo "Mensagem recebida com erro:\n";
+        echo "msg: \t$msg\n";
+        echo "msg2: \t$sensorMessage\n\n";
     }
+}
 
-    $mqtt->debug = false;
+function validateObject($message)
+{
+    return is_object($message) &&
+        property_exists($message, 'number') &&
+        property_exists($message, 'temperature') &&
+        property_exists($message, 'date');
+}
 
-    $topics['SensoresGURIsd'] = array('qos' => 0, 'function' => 'postFirebase');
-    $mqtt->subscribe($topics, 0);
-
-    while($mqtt->proc() && $i<400) {
-        $i++;
-    }
-
-    $mqtt->close();
-
-    function postFirebase($topic, $msg){
-        $sensorMessage = json_decode($msg);
-        if(is_object($sensorMessage) && property_exists($sensorMessage, 'sensorNumber')){
-            $sensor = "Sensor_$sensorMessage->sensorNumber";
-            $temperatura = $sensorMessage->temperatura;
-            $data = $sensorMessage->data;
-            sendCurl($sensor,$temperatura,$data);
-            echo "Mensagem recebida com sucesso, e repassada para Firebase\n";
-        }else{
-            echo "Mensagem recebida com erro:\n";
-            echo "msg: \t$msg\n";
-            echo "msg2: \t$sensorMessage\n\n";
-        }
-        
-    }
-
-    function sendCurl($sensor,$temperatura,$data){
-        $url_banco = getUrlBanco();
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $url_banco.$sensor.".json");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('temperatura'=>$temperatura,'data'=>$data)));
-        curl_exec($ch);
-        curl_close($ch);
-    }
-
-    function getUrlBanco(){
-        return "LINK_DO_SEU_BANCO_FIREBASE";
-    }
-
-?>
+function sendCurl($sensor, $temperature, $date)
+{
+    $url_banco = getUrlBanco();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url_banco . $sensor . ".json");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('temperature' => $temperature, 'date' => $date)));
+    curl_exec($ch);
+    curl_close($ch);
+}
