4930a7f516732daeb16bb3dd6856d85f627f1657
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-12-05 23:06:39 -0300

    feat: adding link and creating place page

--- website/src/components/Place/index.tsx
+++ website/src/components/Place/index.tsx
@@ -8,6 +8,7 @@ import { useDatabaseRef } from "../../hooks/useDatabaseRef";
 
 import { ITemperatureProps } from "./types";
 import styles from "./place.module.css";
+import Link from "next/link";
 
 export interface IPlaceProps {
   name: string;
@@ -30,21 +31,23 @@ export const Place = ({ background, name, dbKey }: IPlaceProps) => {
   }, [placeTemperature]);
 
   return (
-    <div
-      id={dbKey}
-      className={styles.place_single}
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <h2 className={styles.place_name}>{name}</h2>
+    <Link href={{ pathname: `/place`, query: { dbKey } }}>
+      <div
+        id={dbKey}
+        className={styles.place_single}
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <h2 className={styles.place_name}>{name}</h2>
 
-      <div className={styles.place_temperature_container}>
-        <span>
-          {!!temperatures && temperatures.length > 0
-            ? temperatures[0].temperature
-            : "--"}
-          º
-        </span>
+        <div className={styles.place_temperature_container}>
+          <span>
+            {!!temperatures && temperatures.length > 0
+              ? temperatures[0].temperature
+              : "--"}
+            º
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
