909017d668724aa3f85ee854db4f0927ba15bc1d
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-12-13 13:20:58 -0300

    feat: updating esp file

--- ESP32/index.c
+++ ESP32/index.c
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <time.h>
 
 // Replace these with your WiFi credentials
 const char* ssid = "iot";
@@ -18,6 +19,9 @@ const char* mqttTopic = "SensoresGURImc";
 #define DHTTYPE DHT11      // DHT 22 (AM2302) sensor type
 DHT dht(DHTPIN, DHTTYPE);  // Initialize DHT sensor
 
+long timezone = -3;
+byte daysavetime = 1;
+
 // Create an instance of the WiFiClient class
 WiFiClient espClient;
 
@@ -34,8 +38,10 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
-
+  Serial.println("Contacting Time Server");
+  configTime(3600 * timezone, daysavetime * 3600, "time.nist.gov", "0.pool.ntp.org", "1.pool.ntp.org");
   // Connect to MQTT
+  // ntp.setTimeOffset(-10800);
   client.setServer(mqttServer, mqttPort);
 
   // Start the DHT sensor
@@ -58,14 +64,34 @@ void loop() {
     return;
   }
 
-  // Print values to serial monitor
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print(" °C");
+  struct tm tmstruct ;
 
-  // Publish temperature and humidity to MQTT
-  String payload = String(temperature);
-  client.publish(mqttTopic, payload.c_str());
+  tmstruct.tm_year = 0;
+  getLocalTime(&tmstruct);
+
+  String date = (((tmstruct.tm_year) + 1900) + "-" + String(( tmstruct.tm_mon) + 1) + "-" + String(tmstruct.tm_mday));
+  
+  String hour = (String(tmstruct.tm_hour) + ":" + String(tmstruct.tm_min) + ":" + String(tmstruct.tm_sec));
+
+  // Serial.println("Date: " + date + " - Time: " + hour);
+  // Obter os últimos 4 caracteres
+  String dia = date.substring(date.length() - 4);
+  String DateHour = String(dia +" "+ hour);
+  String temperatureString = String(temperature, 1); 
+  String str1 = " {\"number\": \"1\", \"temperature\": \" " + temperatureString + " \", \"date\": \" " + DateHour +" \"} ";
+  String str2 = " {\"number\": \"2\", \"temperature\": \" " + temperatureString + " \", \"date\": \" " + DateHour +" \"} ";
+  String str3 = " {\"number\": \"3\", \"temperature\": \" " + temperatureString + " \", \"date\": \" " + DateHour +" \"} ";
+  String str4 = " {\"number\": \"4\", \"temperature\": \" " + temperatureString + " \", \"date\": \" " + DateHour +" \"} ";
+  //o que vai ser mandado
+  Serial.println(str1);
+  Serial.println(str2);
+  Serial.println(str3);
+  Serial.println(str4);
+  //enviando para o mqtt
+  client.publish(mqttTopic , str1.c_str());
+  client.publish(mqttTopic , str2.c_str());
+  client.publish(mqttTopic , str3.c_str());
+  client.publish(mqttTopic , str4.c_str());
 
   // Delay before the next reading and MQTT publication
   delay(5000);
@@ -77,7 +103,7 @@ void reconnect() {
     Serial.println("Attempting MQTT connection...");
 
     // Attempt to connect
-    if (client.connect("ESP32Client")) {
+    if (client.connect("Teste")) {
       Serial.println("Connected to MQTT");
       // Subscribe to the topic if needed
       // client.subscribe(mqttTopic);
